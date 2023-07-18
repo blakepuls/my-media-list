2b1d99f67f1879e0aebd08b136696f298aa29d35
blakepuls <blake.puls@outlook.com>
2023-07-18 17:25:54 -0400

    Fixed header usermini bug

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,16 +11,16 @@ export default function Header() {
 
   return (
     <header
-      className={`flex w-full p-3 items-center ${
+      className={`relative flex w-full p-3 items-center ${
         pathname.includes("profile") && "bg-gray-900"
       }`}
     >
-      <div className="hidden sm:flex w-1/3 text-left">
+      <div className="hidden sm:block absolute top-3.5 left-3">
         <Link className="text-xl font-bold" href="/">
           MyMediaList
         </Link>
       </div>
-      <nav className="w-full lg:w-1/3 flex gap-1.5 sm:gap-5 sm:justify-center font-medium items-center lg:justify-center text-center">
+      <nav className="absolute top-4 gap-3 flex sm:left-1/2 transform sm:-translate-x-1/2">
         <Link className="transition-colors hover:text-primary-500" href="/">
           Home
         </Link>
@@ -37,7 +37,7 @@ export default function Header() {
           Browse
         </Link>
       </nav>
-      <div className="w-1/3 flex justify-end mr-3 sm:mr-3">
+      <div className="absolute top-2 right-3">
         <UserMini />
       </div>
     </header>
